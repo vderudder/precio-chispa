1aea260f9ac1863d52b5601320f2a78a0c40576b
Victoria Derudder <32909661+vderudder@users.noreply.github.com>
2023-08-25 15:34:17 -0300

    feat: added nuxt labs ui kit

--- prices/nuxt.config.ts
+++ prices/nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  pages: true
+  pages: true,
+  modules: ['@nuxthq/ui']
 })
